483f95a0850bd12d95bb163b4692a8d6ba2c4f10
PlusTM <telekingzarin@gmail.com>
2016-11-11 10:50:33 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,265 @@
+<?php
+define('API_KEY','token');
+//----######------
+function makereq($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+//##############=--API_REQ
+function apiRequest($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+  foreach ($parameters as $key => &$val) {
+    // encoding to JSON array parameters, for example reply_markup
+    if (!is_numeric($val) && !is_string($val)) {
+      $val = json_encode($val);
+    }
+  }
+  $url = "https://api.telegram.org/bot".API_KEY."/".$method.'?'.http_build_query($parameters);
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  return exec_curl_request($handle);
+}
+//----######------
+//---------
+$update = json_decode(file_get_contents('php://input'));
+var_dump($update);
+//=========
+$chat_id = $update->message->chat->id;
+$message_id = $update->message->message_id;
+$from_id = $update->message->from->id;
+$name = $update->message->from->first_name;
+$username = $update->message->from->username;
+$textmessage = isset($update->message->text)?$update->message->text:'';
+$reply = $update->message->reply_to_message->forward_from->id;
+$stickerid = $update->message->reply_to_message->sticker->file_id;
+$admin = 66443035;
+$step = file_get_contents("data/".$from_id."/step.txt");
+
+//-------
+function SendMessage($ChatId, $TextMsg)
+{
+ makereq('sendMessage',[
+'chat_id'=>$ChatId,
+'text'=>$TextMsg,
+'parse_mode'=>"MarkDown"
+]);
+}
+function SendSticker($ChatId, $sticker_ID)
+{
+ makereq('sendSticker',[
+'chat_id'=>$ChatId,
+'sticker'=>$sticker_ID
+]);
+}
+function Forward($KojaShe,$AzKoja,$KodomMSG)
+{
+makereq('ForwardMessage',[
+'chat_id'=>$KojaShe,
+'from_chat_id'=>$AzKoja,
+'message_id'=>$KodomMSG
+]);
+}
+function save($filename,$TXTdata)
+	{
+	$myfile = fopen($filename, "w") or die("Unable to open file!");
+	fwrite($myfile, "$TXTdata");
+	fclose($myfile);
+	}
+//===========
+if ($textmessage == '🔙 برگشت') {
+save("data/$from_id/step.txt","none");
+var_dump(makereq('sendMessage',[
+        	'chat_id'=>$update->message->chat->id,
+        	'text'=>"سلــام 👋😉
+
+🔹 به سرویس پیام رسان تلگرام خوش آمدید 🌹.
+
+🔸 با استفاده از این سرویس شما میتوانید رباتی جهت ارائه پشتیبانی به کاربران ربات، کانال، گروه یا وبسایت خود ایجاد کنید.
+
+🔹برای ساخت ربات از دکمه ی 🔄 ساخت ربات استفاده نمایید.
+
+🤖 @PvSazBot",
+		'parse_mode'=>'MarkDown',
+        	'reply_markup'=>json_encode([
+            	'keyboard'=>[
+                [
+                   ['text'=>"🔄 ساخت ربات"],['text'=>"🚀 ربات های من"],['text'=>"☢ حذف ربات"]
+                ],
+                [
+                   ['text'=>"ℹ️ راهنما"],['text'=>"🔰 قوانین"]
+                ]
+                
+            	],
+            	'resize_keyboard'=>true
+       		])
+    		]));
+}
+elseif ($step == 'create bot') {
+$token = $textmessage ;
+
+			$userbot = json_decode(file_get_contents('https://api.telegram.org/bot'.$token .'/getme'));
+			//==================
+			function objectToArrays( $object ) {
+				if( !is_object( $object ) && !is_array( $object ) )
+				{
+				return $object;
+				}
+				if( is_object( $object ) )
+				{
+				$object = get_object_vars( $object );
+				}
+			return array_map( "objectToArrays", $object );
+			}
+
+	$resultb = objectToArrays($userbot);
+	$un = $resultb["result"]["username"];
+	$ok = $resultb["ok"];
+		if($ok != 1) {
+			//Token Not True
+			SendMessage($chat_id,"توکن نا معتبر!");
+		}
+		else
+		{
+		save("data/$from_id/tedad.txt","0");
+		save("data/$from_id/step.txt","none");
+		SendMessage($chat_id,"در حال ساخت ربات ...");
+		mkdir("bots/$un");
+		mkdir("bots/$un/data");
+		mkdir("bots/$un/data/btn");
+		mkdir("bots/$un/data/words");
+		mkdir("bots/$un/data/profile");
+		mkdir("bots/$un/data/setting");
+		
+		save("bots/$un/data/blocklist.txt","");
+		save("bots/$un/data/last_word.txt","");
+		save("bots/$un/data/pmsend_txt.txt","Message Sent!");
+		save("bots/$un/data/start_txt.txt","Hello World!");
+		save("bots/$un/data/forward_id.txt","");
+		save("bots/$un/data/users.txt","$from_id\n");
+		mkdir("bots/$un/data/$from_id");
+		save("bots/$un/data/$from_id/type.txt","admin");
+		save("bots/$un/data/$from_id/step.txt","none");
+		
+		save("bots/$un/data/btn/btn1_name","");
+		save("bots/$un/data/btn/btn2_name","");
+		save("bots/$un/data/btn/btn3_name","");
+		save("bots/$un/data/btn/btn4_name","");
+		
+		save("bots/$un/data/btn/btn1_post","");
+		save("bots/$un/data/btn/btn2_post","");
+		save("bots/$un/data/btn/btn3_post","");
+		save("bots/$un/data/btn/btn4_post","");
+	
+		save("bots/$un/data/setting/sticker.txt","✅");
+		save("bots/$un/data/setting/video.txt","✅");
+		save("bots/$un/data/setting/voice.txt","✅");
+		save("bots/$un/data/setting/file.txt","✅");
+		save("bots/$un/data/setting/photo.txt","✅");
+		save("bots/$un/data/setting/music.txt","✅");
+		save("bots/$un/data/setting/forward.txt","✅");
+		save("bots/$un/data/setting/joingp.txt","✅");
+		
+		$source = file_get_contents("bot/index.php");
+		$source = str_replace("[*BOTTOKEN*]",$token,$source);
+		$source = str_replace("66443035",$from_id,$source);
+		save("bots/$un/index.php",$source);	
+		file_get_contents("https://api.telegram.org/bot".$token."/setwebhook?url=https://zirgozaronline.ir/tg/PvSazBot/bots/$un/index.php");
+		SendMessage($chat_id,"🚀 ربات شما با موفقیت نصب شده است 
+
+[برای ورود به ربات خود کلیک کنید 😃](https://telegram.me/$un)");
+		}
+}
+elseif (strpos($textmessage , "/toall") !== false ) {
+if ($from_id == $admin) {
+$text = str_replace("/toall","",$textmessage);
+$fp = fopen( "data/users.txt", 'r');
+while( !feof( $fp)) {
+ $users = fgets( $fp);
+SendMessage($users,"$text");
+}
+}
+else {
+SendMessage($chat_id,"You Are Not Admin");
+}
+}
+elseif($textmessage == '/start')
+{
+
+if (!file_exists("data/$from_id/step.txt")) {
+mkdir("data/$from_id");
+save("data/$from_id/step.txt","none");
+save("data/$from_id/tedad.txt","0");
+$myfile2 = fopen("data/users.txt", "a") or die("Unable to open file!");	
+fwrite($myfile2, "$from_id\n");
+fclose($myfile2);
+}
+
+var_dump(makereq('sendMessage',[
+        	'chat_id'=>$update->message->chat->id,
+        	'text'=>"سلــام 👋😉
+
+🔹 به سرویس پیام رسان تلگرام خوش آمدید 🌹.
+
+🔸 با استفاده از این سرویس شما میتوانید رباتی جهت ارائه پشتیبانی به کاربران ربات، کانال، گروه یا وبسایت خود ایجاد کنید.
+
+🔹برای ساخت ربات از دکمه ی 🔄 ساخت ربات استفاده نمایید.
+
+🤖 @PvSazBot",
+		'parse_mode'=>'MarkDown',
+        	'reply_markup'=>json_encode([
+            	'keyboard'=>[
+                [
+                   ['text'=>"🔄 ساخت ربات"],['text'=>"🚀 ربات های من"],['text'=>"☢ حذف ربات"]
+                ],
+                [
+                   ['text'=>"ℹ️ راهنما"],['text'=>"🔰 قوانین"]
+                ]
+                
+            	],
+            	'resize_keyboard'=>true
+       		])
+    		]));
+}
+elseif ($textmessage == '🔄 ساخت ربات') {
+save("data/$from_id/step.txt","create bot");
+var_dump(makereq('sendMessage',[
+        	'chat_id'=>$update->message->chat->id,
+        	'text'=>"توکن را وارد کنید : ",
+		'parse_mode'=>'MarkDown',
+        	'reply_markup'=>json_encode([
+            	'keyboard'=>[
+                [
+                   ['text'=>"🔙 برگشت"]
+                ]
+                
+            	],
+            	'resize_keyboard'=>true
+       		])
+    		]));
+}
+
+else
+{
+SendMessage($chat_id,"Soon ...");
+}
+?>
